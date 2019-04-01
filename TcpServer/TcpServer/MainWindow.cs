e7e7aa0a38f7af23ea3c8fa15289e738b0f43a73
wanghaMAX <wanghamax@gmail.com>
2019-04-02 00:12:32 +0800

    first commit

---
+++ TcpServer/TcpServer/MainWindow.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Gtk;
+using System.Text;
+
+public partial class MainWindow : Gtk.Window
+{
+    TcpListener tcplistener;
+    Thread t;
+    TcpClient client;
+    NetworkStream stream;
+
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+
+    protected void OnConnect(object sender, EventArgs e)
+    {
+        tcplistener = new TcpListener(IPAddress.Any,int.Parse(entry1.Text));
+        tcplistener.Start();
+        t = new Thread(Server)
+        {
+            IsBackground = true
+        };
+        t.Start(tcplistener);
+    }
+
+    private void Server(object o) {
+        TcpListener list = o as TcpListener;
+        label7.Text = "connect succeed";
+        const int bufferSize = 4 * 1024;
+        client = list.AcceptTcpClient();
+        label7.Text = client.Client.RemoteEndPoint.ToString() + " is connected.";
+        while (true)
+        {
+            stream = client.GetStream();
+            byte[] b = new byte[bufferSize];
+            int n = stream.Read(b, 0, bufferSize);
+            string str = Encoding.UTF8.GetString(b);
+            textview2.Buffer.Text = str;
+        }
+    }
+
+    protected void UnConnect(object sender, EventArgs e)
+    {
+        label7.Text = "Nothing is connected.";
+        stream.Close();
+        client.Close();
+        t.Abort();
+    }
+
+    protected void OnSend(object sender, EventArgs e)
+    {
+        if(!IsOnline(client))
+        {
+            label7.Text = "Nothing is connected.";
+        }
+        else
+        {
+            byte[] tex = Encoding.Default.GetBytes(entry2.Text);
+            stream.Write(tex, 0, tex.Length);
+            entry2.Text = "";
+        }
+    }
+    private bool IsOnline(TcpClient c)
+    {
+        return c.Client.Connected;
+    }
+}
---
+++ TcpTest/TcpTest/MainWindow.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using GLib;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+    TcpClient tcpclient;
+    NetworkStream sendStream;
+    System.Threading.Thread thread;
+    int bufferSize = 4 * 1024;
+
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        if(tcpclient != null)
+        {
+            thread.Abort();
+            tcpclient.Close();
+        }
+        Application.Quit();
+        a.RetVal = true;
+    }
+
+    protected void OnConnect(object sender, EventArgs e)
+    {
+        if (entry_ip.Text.Trim() == string.Empty)
+        {
+            return;
+        }
+        if (entry_port.Text.Trim() == string.Empty)
+        {
+            return;
+        }
+        IPAddress ip = IPAddress.Parse(entry_ip.Text);
+        tcpclient = new TcpClient();
+        tcpclient.Connect(ip, int.Parse(entry_port.Text));
+        label1.Text = "start Connect...";
+        if (tcpclient.Connected)
+        {
+            label1.Text = "connect succeed";
+        }
+        else
+        {
+            label1.Text = "connect fail";
+            return;
+        }
+        // get the stream 
+        sendStream = tcpclient.GetStream();
+        // start a thread to receive msg
+        thread = new System.Threading.Thread(new ThreadStart(ListenerServer));
+        thread.Start();
+        }
+
+    protected void OnSend(object sender, EventArgs e)
+    {
+        if (tcpclient != null)
+        {
+            if (entry_send.Text.Trim() != string.Empty)
+            {
+                string msg = entry_send.Text;
+                // put the msg to the buffer
+                byte[] buffer = Encoding.Default.GetBytes(msg);
+                // write the buffer to the stream
+                // lock (sendStream)
+                // {
+                sendStream.Write(buffer, 0, buffer.Length);
+                textview_receive.Buffer.Text = "send :" + msg +"\n";
+                label1.Text = "Send succeed!";
+                // }
+                entry_send.Text = "";
+            }
+            else
+            {
+                label1.Text = "send error";
+                return;
+            }
+        }
+    }
+    public void ListenerServer()
+    {
+        do
+        {
+            try
+            {
+                int readsize;
+                byte[] buffer = new byte[bufferSize];
+                lock (sendStream)
+                {
+                    readsize = sendStream.Read(buffer, 0, bufferSize);
+                }
+                if (readsize == 0)
+                    return;
+                textview_receive.Buffer.Text = "from server:\n"+Encoding.Default.GetString(buffer,0,readsize);
+            }
+            catch {
+                label1.Text = "error";
+            }
+        } while (true);
+    }
+
+    protected void OnDisConnect(object sender, EventArgs e)
+    {
+        tcpclient.Close();
+        thread.Abort();
+    }
+}
